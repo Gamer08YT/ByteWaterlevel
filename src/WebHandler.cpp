cfa65e72e4e27e3b41abec51a538289796daaa6e
Jan Heil <jan.heil@byte-store.de>
2026-01-10 09:31:12 +0100

    "Set up basic web server with LittleFS integration; add root route serving index.html; update platformio.ini with ESPAsyncWebServer dependency"

--- src/WebHandler.cpp
+++ src/WebHandler.cpp
@@ -2,10 +2,30 @@
 // Created by JanHe on 09.01.2026.
 //
 
+#include <Arduino.h>
 #include "WebHandler.h"
+
+#include <LittleFS.h>
+
+#include "ESPAsyncWebServer.h"
+
+// Create AsyncWebServer object on port 80
+AsyncWebServer server(80);
 
 void WebHandler::setup()
 {
+    // Route for root / web page
+    server.on("/", HTTP_GET, [](AsyncWebServerRequest* request)
+    {
+        request->send(LittleFS, "/index.html", String(), false);
+    });
+
+    // Start Webserver.
+    server.begin();
+
+    #if DEBUG == true
+        Serial.println("Webserver started");
+    #endif
 }
 
 void WebHandler::loop()
